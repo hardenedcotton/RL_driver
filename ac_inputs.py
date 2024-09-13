d36696a24c39fad8b5401dc4d0f953a81aeac26e
hardenedcotton <onursener4@gmail.com>
2024-09-13 05:36:48 +0300

    added gaussian dist for sensor placement sensor data is now in ac_inputs

--- ac_inputs.py
+++ ac_inputs.py
@@ -32,51 +32,15 @@ else:
     print("Initialization already done. Skipping setup.")
 
 
-def read_map_config(path):
-    config = configparser.ConfigParser()
-
-    # Read the .ini file
-    config.read(path)
-    parameters = {}
-    if 'PARAMETERS' in config:
-        for key in config['PARAMETERS']:
-            value = config.get('PARAMETERS', key)
-            if value.isdigit():
-                parameters[key] = int(value)
-            elif value.replace('.', '', 1).isdigit():
-                parameters[key] = float(value)
-            else:
-                parameters[key] = value
-
-    return parameters
-
-
-def get_track_attributes():
-    assetto_corsa_root = 'D:\\SteamLibrary\\steamapps\\common\\assettocorsa\\'
-    tracks_root = 'content\\tracks\\'
-
-    current_track = f'{assetto_corsa_root}{tracks_root}{gi.track_name[0]}'
-    if gi.track_name[1]:
-        current_track = f'{current_track}\\{gi.track_name[1]}'
-
-    track_map_path = f'{current_track}\\map.png'
-    map_data_path = f'{current_track}\\data\\map.ini'
-    map_data = read_map_config(map_data_path)
-    map_image = Image.open(track_map_path)
-
-    map_ = {'map_data': map_data,
-            'map_image': map_image}
-
-    return map_
-
-
-def count_attributes(cls):
-    return len([attr for attr in dir(cls) if not attr.startswith('__') and not callable(getattr(cls, attr))])
+def count_info_attributes(cls):
+    return [attr for attr in dir(cls) if not attr.startswith('__') and not callable(getattr(cls, attr))]
 
 
 class GetInfo:
     def __init__(self) -> None:
-        pass
+        current_track = self.get_track_attributes()
+        self.map_data = current_track['map_data']
+        self.map_image = current_track['map_image']
 
     @ property
     def track_name(self):
@@ -189,6 +153,58 @@ class GetInfo:
         '''Returns [x,y,z]'''
         return info.graphics.carCoordinates[:]
 
+    def gauss(self, n, sigma=50, range=(0, 1)):
+        r = np.arange(-int(n/2), int(n/2)+1)
+        result = np.array([1 / (sigma * np.sqrt(2*np.pi)) *
+                          np.exp(-float(x)**2/(2*sigma**2)) for x in r])
+        result = (result - min(result)) / (max(result) - min(result))
+        if range != (0, 1):
+            result = result * (range[1] - range[0]) + range[0]
+        return result
+
+    def read_map_config(self, path):
+        config = configparser.ConfigParser()
+
+        # Read the .ini file
+        config.read(path)
+        parameters = {}
+        if 'PARAMETERS' in config:
+            for key in config['PARAMETERS']:
+                value = config.get('PARAMETERS', key)
+                if value.isdigit():
+                    parameters[key] = int(value)
+                elif value.replace('.', '', 1).isdigit():
+                    parameters[key] = float(value)
+                else:
+                    parameters[key] = value
+
+        return parameters
+
+    def get_track_attributes(self):
+        assetto_corsa_root = 'D:\\SteamLibrary\\steamapps\\common\\assettocorsa\\'
+        tracks_root = 'content\\tracks\\'
+
+        current_track = f'{assetto_corsa_root}{tracks_root}{self.track_name[0]}'
+        if self.track_name[1]:
+            current_track = f'{current_track}\\{self.track_name[1]}'
+
+        track_map_path = f'{current_track}\\map.png'
+        map_data_path = f'{current_track}\\data\\map.ini'
+        map_data = self.read_map_config(map_data_path)
+        map_image = Image.open(track_map_path)
+
+        map_ = {'map_data': map_data,
+                'map_image': map_image}
+
+        return map_
+
+    prev_sensor_mean_angle = 0
+
+    def all_sensors(self):
+        return self.get_sensors()
+    sensors = []
+    triggered_sensors = []
+
     def get_sensors(self, min_sensor_distance=10, min_sensor_count=5, FOV_degrees=90):
         coords = self.coordinates
         heading = self.heading
@@ -202,22 +218,62 @@ class GetInfo:
             int(np.ceil(min_sensor_count * sensor_distance/25) // 2 * 2 + 1), min_sensor_count)
 
         angles = np.linspace(-FOV_radians/2, FOV_radians/2,
-                             sensor_count) + np.radians(90)  # offset is due to headingbeing off by -90 degrees
+                             sensor_count) + np.radians(90)  # offset is due to heading being off by -90 degrees
+        # FIXME Sensor drawer has a problem, i shouldn't really offset it
+        # angles = np.arcsin(angles) * FOV_radians / 2
+        distance_offset = self.gauss(
+            sensor_count, 500/self.speed, (.25, 1))
+        # FIXME The top of the gaussian is sparse, fix it
+        #
         sensors = []
-        for angle in angles:
-            sensor_x = coords[0] + sensor_distance * math.cos(heading + angle)
-            sensor_z = coords[2] + sensor_distance * math.sin(heading + angle)
-            sensors.append((sensor_x, sensor_z))
-        return sensors
+        triggered_sensors = [sensor_distance]
+        for idx, angle in enumerate(angles):
+            sensor_x = self.map_data['x_offset'] + \
+                coords[0] + sensor_distance * distance_offset[idx] * \
+                np.cos(heading + angle) * self.map_data['scale_factor']
+
+            sensor_z = self.map_data['z_offset'] + \
+                coords[2] + sensor_distance * distance_offset[idx] * \
+                np.sin(heading + angle) * self.map_data['scale_factor']
+            is_sensor_on_track = self.is_sensor_on_track(
+                sensor_x, sensor_z)
+
+            sensors.append((sensor_x, sensor_z, angle))
+            if is_sensor_on_track:
+                triggered_sensors.append(angle)
+
+        sensor_mean_angle = np.degrees(
+            np.mean(triggered_sensors[1:]))-90
+
+        if np.isnan(sensor_mean_angle):
+            sensor_mean_angle = self.prev_sensor_mean_angle
+        self.prev_sensor_mean_angle = sensor_mean_angle
+
+        triggered_sensors.append(sensor_mean_angle)
+        return sensors, triggered_sensors
+
+    def is_sensor_on_track(self, x, z):
+        # Check if coordinates are within the bounds of the map image
+        if 0 <= int(x) < self.map_image.width and 0 <= int(z) < self.map_image.height:
+            try:
+                pixel_color = self.map_image.getpixel((int(x), int(z)))
+                alpha_value = pixel_color[3]
+                return True if alpha_value == 255 else False
+
+            except Exception as e:
+                print(f"Error checking pixel color at ({x}, {z}):", e)
+                return False
+        else:
+            return False
 
 
 gi = GetInfo()
 
-current_track = get_track_attributes()
+current_track = gi.get_track_attributes()
 
 
 if __name__ == '__main__':
     print(f'Track: {gi.track_name}')
     print(f'Car: {gi.car_name}')
-    print(f'Class attribute count: {count_attributes(gi)}')
+    print(f'Class attribute count: {len(count_info_attributes(gi))}')
     print(current_track['map_data'])
--- minimap.py
+++ minimap.py
@@ -1,18 +1,22 @@
-import tkinter as tk
 import math
+import tkinter as tk
+
+import numpy as np
 from PIL import Image, ImageTk
-from ac_inputs import GetInfo, get_track_attributes
+
+from ac_inputs import GetInfo
 
 
 class MinimapApp:
     def __init__(self, root):
+        self.gi = GetInfo()
         self.root = root
         self.root.title("Minimap")
         self.canvas = tk.Canvas(
-            root, width=map_data['width'], height=map_data['height'])
+            root, width=self.gi.map_data['width'], height=self.gi.map_data['height'])
         self.canvas.pack()
 
-        self.map_image_tk = ImageTk.PhotoImage(map_image)
+        self.map_image_tk = ImageTk.PhotoImage(self.gi.map_image)
         self.map_image_on_canvas = self.canvas.create_image(
             0, 0, anchor=tk.NW, image=self.map_image_tk)
 
@@ -21,40 +25,41 @@ class MinimapApp:
         self.arrow_on_canvas = None
 
         self.sensors_on_canvas = []
+        self.sensor_info = self.canvas.create_text(
+            50, 20, anchor='nw', text='', font=("Arial", 12), fill="black")
 
+        # self.triggered_sensors = [0]*5
+
+        self.display_data = self.create_data_on_canvas()
         self.update_minimap()
 
-    def update_minimap(self):
+    def draw_car(self):
         if self.arrow_on_canvas:
             self.canvas.delete(self.arrow_on_canvas)
-        for sensor in self.sensors_on_canvas:
-            self.canvas.delete(sensor)
-        self.sensors_on_canvas.clear()
 
-        coords = gi.coordinates
-        heading = gi.heading
-
-        player_x = map_data['x_offset'] + coords[0] * map_data['scale_factor']
-        player_z = map_data['z_offset'] + coords[2] * map_data['scale_factor']
+        player_x = self.gi.map_data['x_offset'] + \
+            self.gi.coordinates[0] * self.gi.map_data['scale_factor']
+        player_z = self.gi.map_data['z_offset'] + \
+            self.gi.coordinates[2] * self.gi.map_data['scale_factor']
 
         arrow_image_rotated = self.arrow_image.rotate(
-            math.degrees(-heading), expand=True)
+            (math.degrees(-self.gi.heading)+180), expand=True)
         self.arrow_image_tk = ImageTk.PhotoImage(arrow_image_rotated)
 
         self.arrow_on_canvas = self.canvas.create_image(
             player_x, player_z, anchor=tk.CENTER, image=self.arrow_image_tk)
 
-        sensors = gi.get_sensors(
-            min_sensor_distance=10, min_sensor_count=15, FOV_degrees=180)
+    def draw_sensors(self, all_sensors, triggered_sensors):
+        for sensor in self.sensors_on_canvas:
+            self.canvas.delete(sensor)
+        self.sensors_on_canvas.clear()
 
-        for sensor in sensors:
-            sensor_x = map_data['x_offset'] + \
-                sensor[0] * map_data['scale_factor']
-            sensor_z = map_data['z_offset'] + \
-                sensor[1] * map_data['scale_factor']
-
-            if self.is_on_track(sensor_x, sensor_z):
+        for sensor in all_sensors:
+            sensor_x = sensor[0]
+            sensor_z = sensor[1]
+            if sensor[2] in triggered_sensors[1:-1]:  # True if sensor is in track
                 sensor_color = "green"
+
             else:
                 sensor_color = "red"
 
@@ -63,32 +68,64 @@ class MinimapApp:
             )
             self.sensors_on_canvas.append(sensor_on_canvas)
 
+    def create_data_on_canvas(self):
+
+        self.data_text_items = []
+        y_offset = 200  # Starting y-position
+
+        data = {
+            'sensor_distance': 0,
+            'sensor_mean_angle': 0,
+            'performance_meter': self.gi.performance_meter,
+            'speed': self.gi.speed,
+            'steer_angle': self.gi.steer_angle,
+            'track_completion': self.gi.track_completion,
+            'wheel_slip_FL': self.gi.wheel_slip[0],
+            'wheel_slip_FR': self.gi.wheel_slip[1],
+            'wheel_slip_RL': self.gi.wheel_slip[2],
+            'wheel_slip_RR': self.gi.wheel_slip[3],
+            'wheels_offtrack': self.gi.wheels_offtrack}
+
+        for i, key in enumerate(data.keys()):
+            text_item = self.canvas.create_text(
+                200, y_offset + i * 20, anchor='w', text=f"{key}: {data[key]}", font=("Arial", 12), fill="black")
+            self.data_text_items.append(text_item)
+
+    def update_data_on_canvas(self, triggered_sensors):
+        sensor_distance = triggered_sensors[0]
+        sensor_mean_angle = triggered_sensors[-1]
+
+        data = {
+            'sensor_distance': sensor_distance,
+            'sensor_mean_angle': sensor_mean_angle,
+            'performance_meter': self.gi.performance_meter,
+            'speed': self.gi.speed,
+            'steer_angle': self.gi.steer_angle,
+            'track_completion': self.gi.track_completion,
+            'wheel_slip_FL': self.gi.wheel_slip[0],
+            'wheel_slip_FR': self.gi.wheel_slip[1],
+            'wheel_slip_RL': self.gi.wheel_slip[2],
+            'wheel_slip_RR': self.gi.wheel_slip[3],
+            'wheels_offtrack': self.gi.wheels_offtrack}
+
+        for i, (key, value) in enumerate(data.items()):
+            # Update each text item with the new value
+            self.canvas.itemconfig(
+                self.data_text_items[i], text=f"{key}: {value}")
+
+    def update_minimap(self):
+
+        all_sensors, triggered_sensors = self.gi.get_sensors(
+            min_sensor_distance=10, min_sensor_count=5, FOV_degrees=90)
+
+        self.draw_car()
+        self.draw_sensors(all_sensors, triggered_sensors)
+        self.update_data_on_canvas(triggered_sensors)
+
         self.root.after(50, self.update_minimap)
 
-    def is_on_track(self, x, z):
-        # Check if coordinates are within the bounds of the map image
-        if 0 <= int(x) < map_image.width and 0 <= int(z) < map_image.height:
-            try:
 
-                pixel_color = map_image.getpixel((int(x), int(z)))
-
-                alpha_value = pixel_color[3]
-
-                if alpha_value == 255:
-                    return True
-                else:
-                    return False
-
-            except Exception as e:
-                print(f"Error checking pixel color at ({x}, {z}):", e)
-                return False
-        else:
-            return False
-
-
-gi = GetInfo()
-map_data = get_track_attributes()['map_data']
-map_image = get_track_attributes()['map_image']
+# gi = GetInfo()
 
 if __name__ == '__main__':
     root = tk.Tk()
